0bbdb31e1b3d78ccd37b5ab4b77b9ddb9ae7a87d
unknown <alstjr0183@naver.com>
2022-02-24 17:55:28 +0900

    Docs: 홈 구성중

---
+++ src/components/Main.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+import { flexbox } from "../styles/utils/flexbox";
+
+const Main: React.FC = () => {
+  return (
+    <MainSection>
+      <Banner></Banner>
+    </MainSection>
+  );
+};
+
+export const MainSection = styled.section``;
+
+export default Main;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,16 @@
 import type { NextPage } from "next";
 import GlobalStyle from "../styles/GlobalStyeld";
-import theme from "../styles/theme"; 
+import theme from "../styles/theme";
 import { ThemeProvider } from "styled-components";
-import Layout from '../components/Layout';
+import Layout from "../components/Layout";
+import Main from "../components/Main";
+
 const Home: NextPage = () => {
   return (
     <>
       <GlobalStyle />
       <ThemeProvider theme={theme}>
+        <Main></Main>
       </ThemeProvider>
     </>
   );
