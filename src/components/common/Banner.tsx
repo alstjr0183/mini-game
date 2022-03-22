7463df9a792eedf198d1f9f94b6260161300c7e7
unknown <alstjr0183@naver.com>
2022-03-22 17:38:16 +0900

    refactor: 배너 수정

--- src/components/common/Banner.tsx
+++ src/components/common/Banner.tsx
@@ -3,7 +3,6 @@ import 'swiper/css/bundle';
 import Image from 'next/image';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore, { Pagination, Navigation } from 'swiper';
-import axios from 'axios';
 import styled from 'styled-components';
 import { Bannerprops } from '../../shared/const';
 
@@ -15,17 +14,17 @@ const Banner = ({ images }: Bannerprops) => {
     <StyledBanner>
       <Swiper
         slidesPerView={1}
-        pagination={{
-          clickable: true,
-        }}
-        navigation
+        // pagination={{
+        //   clickable: true,
+        // }}
+        // navigation
         loop={true}
       >
         {images.map((src: { img: string }, index: number) => {
           console.log(src);
           return (
             <SwiperSlide key={`${index}`}>
-              <Image src={src.img} layout="responsive" width={640} height={400} alt="test_image" />
+              <Image src={src.img} layout="responsive" width={640} height={150} alt="test_image" />
             </SwiperSlide>
           );
         })}
--- src/styles/theme.tsx
+++ src/styles/theme.tsx
@@ -1,5 +1,15 @@
-import styled, { css } from "styled-components";
+import styled, { css } from 'styled-components';
 
-const theme = {};
+const deviceSizes: { mobile: number; desktop: number } = {
+  mobile: 1020,
+  desktop: 1920,
+};
+
+const device: { mobile: string; desktop: string } = {
+  mobile: `(max-width : ${deviceSizes.mobile}px)`,
+  desktop: `(min-width : ${deviceSizes.mobile + 1}px)`,
+};
+
+const theme = { device };
 
 export default theme;
