d67bd83303a2cc445a15b7e83efe4c69c54b41a4
yerin512 <sosu0512@naver.com>
2022-03-22 17:30:36 +0900

    Style gamelist

--- src/components/GameList.tsx
+++ src/components/GameList.tsx
@@ -6,30 +6,65 @@ import styled from 'styled-components';
 export default function GameList() {
   return (
     <Container>
-      <ul>
-        <li>
+      <ul className="game__list">
+        <li className="first__line game__name ">
           <Link href={{ pathname: '/games/runningman' }}>
             <a>런닝록맨</a>
           </Link>
         </li>
-        <li>
-          <Link href={{ pathname: '/games/runningman' }}>
-            <a>가위바위보</a>
-          </Link>
-        </li>
-        <li>
-          <Link href={{ pathname: '/games/runningman' }}>
-            <a>오목</a>
-          </Link>
-        </li>
-        <li>
-          <Link href={{ pathname: '/games/runningman' }}>
-            <a>두더지 잡기</a>
-          </Link>
-        </li>
+        <div className="second__line">
+          <li className="game__name">
+            <Link href={{ pathname: '/games/runningman' }}>
+              <a>가위바위보</a>
+            </Link>
+          </li>
+          <li className="game__name">
+            <Link href={{ pathname: '/games/runningman' }}>
+              <a>오목</a>
+            </Link>
+          </li>
+          <li className="game__name">
+            <Link href={{ pathname: '/games/runningman' }}>
+              <a>두더지 잡기</a>
+            </Link>
+          </li>
+        </div>
       </ul>
     </Container>
   );
 }
 
-const Container = styled.div``;
+const Container = styled.div`
+  width: 100%;
+
+  .game__list {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+
+    .game__name {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      cursor: pointer;
+      height: 100px;
+      background-color: #f2f2f2;
+      font-family: 'Jal_Haru';
+    }
+
+    .first__line {
+      margin: 10px;
+      background-color: #c1c1c1;
+    }
+    .second__line {
+      width: 100%;
+      display: flex;
+      flex: 1;
+
+      .game__name {
+        flex: 1;
+        margin: 10px;
+      }
+    }
+  }
+`;
