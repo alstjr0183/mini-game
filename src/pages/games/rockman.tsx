c1c9d9353ac15bf1963ceb37fc39be187d86f48f
yerin512 <sosu0512@naver.com>
2022-04-08 10:48:29 +0900

    Etc: 컴포넌트명 수정

--- src/pages/games/rockman.tsx
+++ src/pages/games/rockman.tsx
@@ -1,16 +1,13 @@
 import { NEXT_REQUEST_META } from 'next/dist/server/request-meta';
 import { Tracing } from 'trace_events';
 import HeadInfo from '../../components/HeaderInfo';
-import Rockman from '../../components/rockman/index';
+import Rockman from '../../components/rockman/Index';
 
 const rockman = () => {
   return (
     <div>
       <HeadInfo title="런닝록맨" />
-      <h1>런닝녹맨</h1>
-      dd
       <Rockman />
-      dd
     </div>
   );
 };
