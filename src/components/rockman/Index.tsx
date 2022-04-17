bb8fc30f38af26bc7bef4f0ecb9afb83b1a17a83
yerin512 <sosu0512@naver.com>
2022-04-18 08:47:08 +0900

    Feat: 장애물 생성

--- src/components/rockman/Index.tsx
+++ src/components/rockman/Index.tsx
@@ -1,9 +1,11 @@
+import { disconnect } from 'process';
 import React, { useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
 interface CanvasSize {
   WIDTH: number;
   HEIGHT: number;
 }
+
 function RockMan(): any {
   const [canvas, setCanvas] = useState<HTMLCanvasElement | null>(null);
   const [ctx, setCtx] = useState<CanvasRenderingContext2D | null>(null);
@@ -13,6 +15,60 @@ function RockMan(): any {
     HEIGHT: 0,
   });
 
+  const rockman = {
+    x: 10,
+    y: 200,
+    width: 50,
+    height: 50,
+    draw() {
+      if (!ctx) return;
+      ctx.fillStyle = 'red';
+      ctx.fillRect(this.x, this.y, this.width, this.height);
+    },
+  };
+
+  class Cactus {
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+    constructor() {
+      this.x = size.WIDTH;
+      this.y = 200;
+      this.width = 50;
+      this.height = 50;
+    }
+    draw() {
+      if (!ctx) return;
+      ctx.fillStyle = 'green';
+      ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+  }
+
+  var cactus = new Cactus(); //장애물
+  var cactuses: any[] = [];
+  var timer = 0;
+
+  // 이동하는 함수
+  function 프레임마다실행할거() {
+    requestAnimationFrame(프레임마다실행할거);
+    timer++;
+
+    ctx?.clearRect(0, 0, window.innerWidth, window.innerHeight);
+
+    if (timer % 140 === 0) {
+      var cactus = new Cactus();
+      cactuses.push(cactus);
+      cactus.draw();
+    }
+    cactuses.forEach((a) => {
+      a.x--;
+      a.draw();
+    });
+
+    rockman.draw();
+  }
+
   useEffect(() => {
     if (!canvasRef.current) return;
     const canvas = canvasRef.current;
@@ -21,9 +77,8 @@ function RockMan(): any {
   }, []);
 
   useEffect(() => {
-    if (!ctx) return;
-    ctx.fillStyle = 'red';
-    ctx.fillRect(0, 0, 200, 200);
+    프레임마다실행할거();
+    cactus.draw();
   }, [canvas, ctx]);
 
   useEffect(() => {
@@ -33,13 +88,6 @@ function RockMan(): any {
       HEIGHT: window.innerHeight,
     });
   }, []);
-
-  const rockman = {
-    x: 10,
-    y: 200,
-    width: 50,
-    height: 50,
-  };
 
   return (
     <Container>
