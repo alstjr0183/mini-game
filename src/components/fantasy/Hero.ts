5c2d20a5a10d2f0a35667fdc234c419062c80903
unknown <alstjr0183@naver.com>
2022-04-21 17:09:02 +0900

    feat: 영웅의 스킬 몬스터쪽으로 자동 발사 기능 추가완료

--- src/components/fantasy/Hero.ts
+++ src/components/fantasy/Hero.ts
@@ -1,40 +1,34 @@
-export interface Hero {
-  hero: HeroTypes;
+import { HeroTypes, HeroState, Event } from '../../types/fantasy/hero';
+
+export default class Hero {
+  hero: HeroState;
   setHero: any;
-  move: number;
-  e: event;
-}
-
-interface event {
-  key: string;
-}
-
-interface HeroTypes {
-  xPos: number;
-  yPos: number;
-}
-
-// 영웅 방향키 조작 이벤트
-export const keyDownHandler = ({ e, hero, setHero, move }: Hero) => {
-  if (e.key === 'd') {
-    setHero({
-      ...hero,
-      xPos: hero.xPos + move,
-    });
-  } else if (e.key === 'a') {
-    setHero({
-      ...hero,
-      xPos: hero.xPos - move,
-    });
-  } else if (e.key === 'w') {
-    setHero({
-      ...hero,
-      yPos: hero.yPos - move,
-    });
-  } else if (e.key === 's') {
-    setHero({
-      ...hero,
-      yPos: hero.yPos + move,
-    });
+  constructor(hero: HeroState, setHero: any) {
+    this.hero = hero;
+    this.setHero = setHero;
   }
-};
+  // 영웅 방향키 조작 이벤트
+  keyDownHandler = (e: Event) => {
+    if (e.key === 'd') {
+      this.setHero({
+        ...this.hero,
+        xPos: this.hero.xPos + this.hero.speed,
+      });
+    } else if (e.key === 'a') {
+      this.setHero({
+        ...this.hero,
+        xPos: this.hero.xPos - this.hero.speed,
+      });
+    } else if (e.key === 'w') {
+      this.setHero({
+        ...this.hero,
+        yPos: this.hero.yPos - this.hero.speed,
+      });
+    } else if (e.key === 's') {
+      this.setHero({
+        ...this.hero,
+        yPos: this.hero.yPos + this.hero.speed,
+      });
+    }
+  };
+}
--- src/components/fantasy/Index.tsx
+++ src/components/fantasy/Index.tsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
 import { Size } from '../../types/fantasy';
-import { Hero, keyDownHandler } from './Hero';
+import { HeroTypes } from '../../types/fantasy/hero';
+import Hero from './Hero';
 import magic from '../../images/fantasy/magic.png';
 
 const Index = () => {
+  // -----------------state------------------------
   const [canvas, setCanvas] = useState<HTMLCanvasElement | null>(null);
   const [ctx, setCtx] = useState<CanvasRenderingContext2D | null>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
@@ -12,24 +14,50 @@ const Index = () => {
     width: 0,
     height: 0,
   });
-
+  const [setting, setSetting] = useState({});
+  // 영웅 state
+  const [skill, setSkill] = useState({
+    start: true,
+    speed: 20,
+    xPos: 0,
+    yPos: 0,
+    size: 20,
+  });
   const [hero, setHero] = useState({
-    xPos: 0,
-    yPos: 0,
+    speed: 10,
+    xPos: 800,
+    yPos: 800,
+    level: 1,
+    damage: 10,
+    experience: 0,
   });
-
+  // 몬스터 state
   const [monster, setMonster] = useState({
-    xPos: 0,
-    yPos: 0,
+    speed: 5,
+    xPos: 100,
+    yPos: 100,
   });
-
-  // const [xPos, setxPos] = useState(10);
-  // const [yPos, setyPos] = useState(10);
 
   const { width, height } = size;
+  const heroClass = new Hero(hero, setHero);
 
-  const move = 50;
+  // 따라 가는 로직
+  const f_follow = (first: number, second: number, speed: number): number => {
+    if (first < second) {
+      // 따라 가는 대상의 좌표가 더 낮다면?
+      return second - speed;
+    } else {
+      // 따라 가는 대상의 좌표가 더 크다면?
+      return second + speed;
+    }
+  };
 
+  // 근처에 왔는지 감지 로직
+  const f_nearApproach = (firstX: number, firstY: number, secondX: number, secondY: number, interval: number) => {
+    return Math.abs(firstX - secondX) < interval && Math.abs(firstY - secondY) < interval ? true : false;
+  };
+
+  // 캔버스 생성
   useEffect(() => {
     setSize({
       width: window.innerWidth,
@@ -37,73 +65,83 @@ const Index = () => {
     });
     if (!canvasRef.current) return;
     const canvas = canvasRef.current;
+
     setCanvas(canvas);
     setCtx(canvas.getContext('2d'));
   }, []);
 
-  // setInterval(() => {
-  //   const heroX = hero.xPos / 100;
-  //   const heroY = hero.yPos / 100;
-  //   setMonster({
-  //     ...monster,
-  //     xPos: monster.xPos + heroX,
-  //     yPos: monster.yPos + heroY,
-  //   });
-  // }, 1000);
-
-  console.log(monster);
-
+  // 캔버스 애니메이션
   useEffect(() => {
     if (!ctx) return;
-    const test = () => {
+    const draw = () => {
       ctx.clearRect(0, 0, width, height);
 
       const heroImg = new Image();
       const monsterImg = new Image();
       heroImg.src = '/magic.png';
       monsterImg.src = '/monster.png';
+
+      // 영웅 로직
       ctx.drawImage(heroImg, hero.xPos, hero.yPos, 100, 100);
+
+      // 몬스터 로직
       ctx.drawImage(monsterImg, monster.xPos, monster.yPos, 100, 100);
+
+      // 스킬 로직
+      if (skill.start) {
+        console.log('처음');
+        setSkill((prevSkill) => ({
+          ...prevSkill,
+          start: false,
+          xPos: hero.xPos,
+          yPos: hero.yPos,
+          size: 15,
+        }));
+      }
+      ctx.beginPath();
+      ctx.arc(skill.xPos, skill.yPos, skill.size, 0, 2 * Math.PI);
+      ctx.fillStyle = 'rgb(255,221,0)';
+      ctx.fill();
+
+      if (!skill.start) {
+        console.log('2');
+        setSkill((prevSkill) => ({
+          ...prevSkill,
+          xPos: f_follow(monster.xPos, skill.xPos, skill.speed),
+          yPos: f_follow(monster.yPos, skill.yPos, skill.speed),
+        }));
+      }
+
+      if (f_nearApproach(monster.xPos, monster.yPos, skill.xPos, skill.yPos, 30)) {
+        setSkill((prevSkill) => ({
+          ...prevSkill,
+          start: true,
+          size: 0,
+        }));
+      }
     };
 
-    requestAnimationFrame(test);
-  }, [ctx, hero.xPos, hero.yPos, monster]);
+    requestAnimationFrame(draw);
+  }, [ctx, monster]);
 
-  const f_tt = () => {
-    const heroX = hero.xPos / 10;
-    const heroY = hero.yPos / 10;
-    setMonster({
-      ...monster,
-      xPos: monster.xPos + heroX,
-      yPos: monster.yPos + heroY,
-    });
-  };
-
+  console.log(skill);
+  // 몬스터가 영웅 좌표로 따라다니는 로직
   useEffect(() => {
     const timer = setInterval(() => {
-      const heroX = hero.xPos / 5;
-      const heroY = hero.yPos / 5;
-
-      const monsterX = hero.xPos < monster.xPos ? monster.xPos - heroX : monster.xPos + heroX;
-      const monsterY = hero.yPos < monster.yPos ? monster.yPos - heroY : monster.yPos + heroY;
       setMonster((prevmonster) => ({
         ...prevmonster,
-        xPos: monsterX,
-        yPos: monsterY,
+        xPos: f_follow(hero.xPos, monster.xPos, monster.speed),
+        yPos: f_follow(hero.yPos, monster.yPos, monster.speed),
       }));
-    }, 1000);
-
+    }, 50);
     return () => clearInterval(timer);
   }, [monster]);
-
-  // setInterval(f_tt, 1000);
 
   return (
     <StyledIndex>
       <canvas
         onKeyDown={(e) => {
-          const t: Hero = { e: e, hero: hero, setHero: setHero, move: move };
-          keyDownHandler(t);
+          heroClass.keyDownHandler(e);
         }}
         tabIndex={0}
         width={width}
